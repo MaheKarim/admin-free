be7ffe7b35756c1c3343f083683b9108dd65d31b
MaheKarim <mahekarim@gmail.com>
2019-09-06 19:57:32 +0600

    Update

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'postgres'),
+            'host' => env('DB_HOST', 'postgre'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'root'),
