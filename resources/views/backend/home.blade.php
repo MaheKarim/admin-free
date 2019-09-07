430358b5e2438b33e7affdf0a002de1e29d68a6a
MaheKarim <mahekarim@gmail.com>
2019-09-07 23:58:30 +0600

    Asset Clear

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -144,7 +144,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{ asset('/') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="{{ asset('/') }}dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -154,7 +154,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('/') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ asset('/') }}dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -895,7 +895,7 @@
                           <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="{{ asset('/') }}/dist/img/user1-128x128.jpg" alt="message user image">
+                        <img class="direct-chat-img" src="{{ asset('/') }}dist/img/user1-128x128.jpg" alt="message user image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                           Is this template really for free? That's unbelievable!
@@ -911,7 +911,7 @@
                           <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="{{ asset('/') }}/dist/img/user3-128x128.jpg" alt="message user image">
+                        <img class="direct-chat-img" src="{{ asset('/') }}dist/img/user3-128x128.jpg" alt="message user image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                           You better believe it!
@@ -927,7 +927,7 @@
                           <span class="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="{{ asset('/') }}/dist/img/user1-128x128.jpg" alt="message user image">
+                        <img class="direct-chat-img" src="{{ asset('/') }}dist/img/user1-128x128.jpg" alt="message user image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                           Working with AdminLTE on a great new app! Wanna join?
@@ -943,7 +943,7 @@
                           <span class="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="{{ asset('/') }}/dist/img/user3-128x128.jpg" alt="message user image">
+                        <img class="direct-chat-img" src="{{ asset('/') }}dist/img/user3-128x128.jpg" alt="message user image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                           I would love to.
@@ -960,7 +960,7 @@
                       <ul class="contacts-list">
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="{{ asset('/') }}/dist/img/user1-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('/') }}dist/img/user1-128x128.jpg">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -975,7 +975,7 @@
                         <!-- End Contact Item -->
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="{{ asset('/') }}/dist/img/user7-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('/') }}dist/img/user7-128x128.jpg">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -990,7 +990,7 @@
                         <!-- End Contact Item -->
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="{{ asset('/') }}/dist/img/user3-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('/') }}dist/img/user3-128x128.jpg">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -1005,7 +1005,7 @@
                         <!-- End Contact Item -->
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="{{ asset('/') }}/dist/img/user5-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('/') }}dist/img/user5-128x128.jpg">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -1020,7 +1020,7 @@
                         <!-- End Contact Item -->
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="{{ asset('/') }}/dist/img/user6-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('/') }}dist/img/user6-128x128.jpg">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -1035,7 +1035,7 @@
                         <!-- End Contact Item -->
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="{{ asset('/') }}/dist/img/user8-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('/') }}dist/img/user8-128x128.jpg">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -1103,27 +1103,27 @@
                         <span class="users-list-date">12 Jan</span>
                       </li>
                       <li>
-                        <img src="{{ asset('/') }}/dist/img/user6-128x128.jpg" alt="User Image">
+                        <img src="{{ asset('/') }}dist/img/user6-128x128.jpg" alt="User Image">
                         <a class="users-list-name" href="#">John</a>
                         <span class="users-list-date">12 Jan</span>
                       </li>
                       <li>
-                        <img src="{{ asset('/') }}/dist/img/user2-160x160.jpg" alt="User Image">
+                        <img src="{{ asset('/') }}dist/img/user2-160x160.jpg" alt="User Image">
                         <a class="users-list-name" href="#">Alexander</a>
                         <span class="users-list-date">13 Jan</span>
                       </li>
                       <li>
-                        <img src="{{ asset('/') }}/dist/img/user5-128x128.jpg" alt="User Image">
+                        <img src="{{ asset('/') }}dist/img/user5-128x128.jpg" alt="User Image">
                         <a class="users-list-name" href="#">Sarah</a>
                         <span class="users-list-date">14 Jan</span>
                       </li>
                       <li>
-                        <img src="{{ asset('/') }}/dist/img/user4-128x128.jpg" alt="User Image">
+                        <img src="{{ asset('/') }}dist/img/user4-128x128.jpg" alt="User Image">
                         <a class="users-list-name" href="#">Nora</a>
                         <span class="users-list-date">15 Jan</span>
                       </li>
                       <li>
-                        <img src="{{ asset('/') }}/dist/img/user3-128x128.jpg" alt="User Image">
+                        <img src="{{ asset('/') }}dist/img/user3-128x128.jpg" alt="User Image">
                         <a class="users-list-name" href="#">Nadia</a>
                         <span class="users-list-date">15 Jan</span>
                       </li>
@@ -1371,7 +1371,7 @@
                 <ul class="products-list product-list-in-card pl-2 pr-2">
                   <li class="item">
                     <div class="product-img">
-                      <img src="{{ asset('/') }}/dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
+                      <img src="{{ asset('/') }}dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
                     </div>
                     <div class="product-info">
                       <a href="javascript:void(0)" class="product-title">Samsung TV
@@ -1384,7 +1384,7 @@
                   <!-- /.item -->
                   <li class="item">
                     <div class="product-img">
-                      <img src="{{ asset('/') }}/dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
+                      <img src="{{ asset('/') }}dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
                     </div>
                     <div class="product-info">
                       <a href="javascript:void(0)" class="product-title">Bicycle
@@ -1397,7 +1397,7 @@
                   <!-- /.item -->
                   <li class="item">
                     <div class="product-img">
-                      <img src="{{ asset('/') }}/dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
+                      <img src="{{ asset('/') }}dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
                     </div>
                     <div class="product-info">
                       <a href="javascript:void(0)" class="product-title">
@@ -1413,7 +1413,7 @@
                   <!-- /.item -->
                   <li class="item">
                     <div class="product-img">
-                      <img src="{{ asset('/') }}/dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
+                      <img src="{{ asset('/') }}dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
                     </div>
                     <div class="product-info">
                       <a href="javascript:void(0)" class="product-title">PlayStation 4
@@ -1462,27 +1462,27 @@
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
-<script src="{{ asset('/') }}/plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('/') }}plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="{{ asset('/') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('/') }}plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="{{ asset('/') }}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{ asset('/') }}plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.js"></script>
 
 <!-- OPTIONAL SCRIPTS -->
-<script src="{{ asset('/') }}/dist/js/demo.js"></script>
+<script src="{{ asset('/') }}dist/js/demo.js"></script>
 
 <!-- PAGE PLUGINS -->
 <!-- jQuery Mapael -->
-<script src="{{ asset('/') }}/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="{{ asset('/') }}/plugins/raphael/raphael.min.js"></script>
-<script src="{{ asset('/') }}/plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="{{ asset('/') }}/plugins/jquery-mapael/maps/world_countries.min.js"></script>
+<script src="{{ asset('/') }}plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="{{ asset('/') }}plugins/raphael/raphael.min.js"></script>
+<script src="{{ asset('/') }}plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="{{ asset('/') }}plugins/jquery-mapael/maps/world_countries.min.js"></script>
 <!-- ChartJS -->
 <script src="plugins/chart.js/Chart.min.js"></script>
 
 <!-- PAGE SCRIPTS -->
-<script src="{{ asset('/') }}/dist/js/pages/dashboard2.js"></script>
+<script src="{{ asset('/') }}dist/js/pages/dashboard2.js"></script>
 </body>
 </html>
